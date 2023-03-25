ef3053c18135b6f2c33097c3a3aeffcce87e436c
Dabaiee <tongzhou1212@hotmail.com>
2023-03-25 16:18:25 -0700

    connect wallet

--- Onchain_Invoicing/src/modules/ConnectWallet/ConnectWallet.jsx
+++ Onchain_Invoicing/src/modules/ConnectWallet/ConnectWallet.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import "./ConnectWallet.css";
+import Web3 from 'web3';
 
 const mockNetworkList = ["Network", "OKX", "ETC", "MATIC"];
 
 const Login = () => {
   const [selectedNetwork, setSelectedNetwork] = useState(mockNetworkList[0]);
+    async function connectWallet() {
+        try {
+            if (typeof window.ethereum === 'undefined') {
+                alert('Please install MetaMask or another Web3 provider.');
+                return;
+            }
+
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setAccount(accounts[0]);
+            await initializeWeb3(accounts[0]);
+        } catch (error) {
+            console.error('Error connecting to wallet:', error);
+        }
+    }
 
   return (
     <div>
@@ -24,7 +39,9 @@ const Login = () => {
           </option>
         ))}
       </select>
-      <button className="connectWallet_button">Connect Wallet</button>
+      <button className="connectWallet_button"
+              onClick={connectWallet}
+      >Connect Wallet</button>
     </div>
   );
 };
