5edfe1d0c2e1feda0cdb26990430661630d5c0fe
Dabaiee <tongzhou1212@hotmail.com>
2023-03-26 05:52:45 -0700

    paid

--- Onchain_Invoicing/src/modules/Paid_Invoicing/Paid_Invoicing.jsx
+++ Onchain_Invoicing/src/modules/Paid_Invoicing/Paid_Invoicing.jsx
@@ -49,17 +49,17 @@ const PaidInvoicing = () => {
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
                   >
-                    {invoice.invoiceId}
+                    {invoice[0]}
                   </td>
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
                   >
-                    {invoice.date}
+                    {Date(invoice[3]* 1000)}
                   </td>
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
                   >
-                    {invoice.poNumber}
+                    {invoice[7]}
                   </td>
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
@@ -69,7 +69,7 @@ const PaidInvoicing = () => {
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
                   >
-                    {invoice.clientWallet}
+                    {invoice[2]}
                   </td>
                   <td
                     style={{ border: "1px solid black", gap: 30, width: "14%" }}
